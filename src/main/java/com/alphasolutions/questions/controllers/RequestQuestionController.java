0e6cd6a66e64074c33d70a261ee51f7f7cb761fa
Andre-PI <andrelucas_PI@proton.me>
2024-10-19 15:48:00 -0300

    Json return

--- src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
+++ src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
@@ -3,15 +3,18 @@ package com.alphasolutions.questions.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alphasolutions.questions.model.Question;
 import com.alphasolutions.questions.model.QuestionDAO;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @RestController
@@ -23,12 +26,18 @@ public class RequestQuestionController {
     @Autowired
     QuestionDAO questionDAO;
 
-    @GetMapping("/getquestion")
-    public ResponseEntity<String> getQuestion(@RequestHeader(value = "x-api-key") String key, @RequestParam String topic) {
+    @GetMapping(path = "/getquestion", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getQuestion(@RequestHeader(value = "x-api-key") String key, @RequestParam String topic) {
         if(!apiKey.equals(key)){
             return ResponseEntity.badRequest().body("Negado");
         }
-        return ResponseEntity.ok().body(questionDAO.findByTopic(topic).toString());
+
+        List<Question> question = questionDAO.findByTopic(topic);
+        
+        if(question == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(question);
     }
     
 }
