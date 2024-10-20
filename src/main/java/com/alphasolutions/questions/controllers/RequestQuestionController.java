62c67c36e909b3be5a8018a2c7785e0a5ef440bc
Andre-PI <andrelucas_PI@proton.me>
2024-10-19 23:50:21 -0300

    update json

--- src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
+++ src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alphasolutions.questions.model.Question;
 import com.alphasolutions.questions.model.QuestionDAO;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,20 +27,17 @@ public class RequestQuestionController {
     QuestionDAO questionDAO;
 
     @GetMapping(path = "/getquestion",produces = MediaType.APPLICATION_JSON_VALUE)
-    public Question getQuestion(@RequestHeader(value = "x-api-key") String key, @RequestParam String topic)  {
+    public ResponseEntity<?> getQuestion(@RequestHeader(value = "x-api-key") String key, @RequestParam String topic)  {
         if(!apiKey.equals(key)){
-            return null;
+            return ResponseEntity.badRequest().body("Negado, verifique a api key");
         }
 
         List<Question> questions = questionDAO.findByTopic(topic);
         
         if(questions.isEmpty()){
-            return null;
+            return ResponseEntity.badRequest().body("Não tem esse tipo de questão ainda");
         }
-        Question question = questions.get(0);
-        
-        return new Question(question.getQuestion(),question.getAnswer(),question.getWrongAnswer(),question.getSecondWrongAnswer(),question.getTopic());
-
+        return ResponseEntity.ok(questions);
     }
     
 }
