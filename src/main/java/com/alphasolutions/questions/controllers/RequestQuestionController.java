d9c8dbe405bd304eb93893f0bdbbc48852479649
Andre-PI <andrelucas_PI@proton.me>
2024-10-20 12:06:32 -0300

    addition of the pulsar

--- src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
+++ src/main/java/com/alphasolutions/questions/controllers/RequestQuestionController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -39,5 +40,11 @@ public class RequestQuestionController {
         }
         return ResponseEntity.ok(questions);
     }
+
+    @PostMapping("/question")
+    public ResponseEntity<?> reviveTheSystem() {
+        return ResponseEntity.ok("Pulsar sended");
+    }
+    
     
 }
